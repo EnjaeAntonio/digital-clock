7f04287223ab0fd81775afbc5fd25abc98fe9f36
Enjae Antonio <Enjaeantonio@outlook.com>
2022-11-18 13:56:23 -0600

    setting alarm

--- assets/script/index.js
+++ assets/script/index.js
@@ -23,10 +23,13 @@ function select(selector, parent = document) {
    console.log(content);
  }
 
-const display = select('.display h1')
-const output = select('.output p')
+const display = select('.display h1');
+const output = select('.output p');
+const setAlarm = select('.alarm-btn')
+const enterAlarm = select('#enteralarm');
+const alarmSound = ('OOJ\Enjae_Antonio_OOP_Assignment_one\assets\img\Wecker-sound.mp3');
 
-const digitalClock = setInterval(() => {
+const clock = setInterval(() => {
     let todaysDate = new Date();
 
     let hr = todaysDate.getHours().toString();
@@ -34,5 +37,16 @@ const digitalClock = setInterval(() => {
     let sec = todaysDate.getSeconds().toString().padStart(2, '0');
 
     display.innerText = `${hr}: ${min}: ${sec}`
-    
+
 }, 1000 )
+
+onEvent('click', setAlarm, function(){
+    let a = enterAlarm.value;
+    
+    output.innerText = `${a}`
+    if (output.innerText === display.innerText) {
+
+    }
+});
+
+alarmSound.play()
