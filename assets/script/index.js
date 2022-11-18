7ab8e19d86bc5efad1d992f4fe74a31bd08a7e2e
Enjae Antonio <Enjaeantonio@outlook.com>
2022-11-18 13:06:16 -0600

    added files and basic html

---
+++ assets/script/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// Utility Functions
+function onEvent(event, selector, callback) {
+    return selector.addEventListener(event, callback);
+ }
+    
+
+function select(selector, parent = document) {
+    return parent.querySelector(selector);
+ }
+
+ function selectAll(selector, parent = document) {
+    return parent.querySelectorAll(selector);
+ }
+
+
+ function create(element, parent = document) {
+   return parent.createElement(element);
+ }
+
+ function log(content) {
+   console.log(content);
+ }
+
+const hour = select('.hour')
+const minute = select('.minute')
+const seconds = select('.seconds')
+
+function clock() {
+    let todaysDate = new Date();
+
+    let hr = 
+}
