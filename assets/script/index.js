810985ef7c2b33c6cd9ab154452c540a7e4abeb8
Enjae Antonio <Enjaeantonio@outlook.com>
2022-11-18 16:14:50 -0600

    got alarm to play

--- assets/script/index.js
+++ assets/script/index.js
@@ -28,9 +28,9 @@ const output = select('.output p');
 const hours = select('.hours');
 const minutes = select('.minutes');
 const seconds = select('.seconds')
-const alarmBtn = select('.alarm-btn')
+const setAlarm = select('.alarm-btn')
 const enterAlarm = select('#enteralarm');
-const alarmSound = ('/alarm.mp3');
+const alarmSound = new Audio('./assets/img/alarm.mp3');
 
 
 const clock = setInterval(() => {
@@ -40,12 +40,47 @@ const clock = setInterval(() => {
     let min = todaysDate.getMinutes().toString().padStart(2, '0');
     let sec = todaysDate.getSeconds().toString().padStart(2, '0');
 
-    display.innerText = `${hr}: ${min}: ${sec}`
+    if (hr > 12) {
+        hr -= 12;
+    } else if (hr === 0) {
+       hr = 12;
+    }
+
+    display.innerText = hr + ':' + min + ':' + sec;
+
 
 }, 1000 );
 
-onEvent('click', alarmBtn, function(){
-    let inputHours = hours.value;
-    let inputMinutes = minutes.value;
-    let inputSeconds = seconds.value;
-} )
+onEvent('click', setAlarm, function(){
+    let alarmHours = hours.value;
+    let alarmMinutes = minutes.value;
+    let alarmSeconds = seconds.value;
+    let alarmTime = alarmHours + ':' + alarmMinutes + ':' + alarmSeconds;
+    console.log(alarmTime)
+
+    setInterval(() => {
+        let todaysDate = new Date();
+    
+        let hr = todaysDate.getHours().toString().padStart(2, '0');
+        let min = todaysDate.getMinutes().toString().padStart(2, '0');
+        let sec = todaysDate.getSeconds().toString().padStart(2, '0');
+    
+        if (hr > 12) {
+            hr -= 12;
+        } else if (hr === 0) {
+           hr = 12;
+        }
+    
+       let currentTime =  display.innerText = hr + ':' + min + ':' + sec;
+    
+           if (alarmTime === currentTime){
+                alarmSound.play();
+           }
+    }, 1000 );
+});
+
+
+
+
+
+
