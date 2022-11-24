bbc1deb6035023695badcee17d38906cb155d47d
Enjae Antonio <Enjaeantonio@outlook.com>
2022-11-24 09:54:14 -0600

    removed seconds input

--- assets/script/index.js
+++ assets/script/index.js
@@ -51,8 +51,7 @@ onEvent('click', setAlarm, function(){
     
     let alarmHours = hours.value.toString().padStart(2, '0');
     let alarmMinutes = minutes.value.toString().padStart(2, '0');
-    let alarmSeconds = seconds.value.toString().padStart(2, '0');
-    let alarmTime = alarmHours + ':' + alarmMinutes + ':' + alarmSeconds;
+    let alarmTime = alarmHours + ':' + alarmMinutes + ':' + '00';
 
    
 
@@ -62,10 +61,11 @@ onEvent('click', setAlarm, function(){
         let hr = todaysDate.getHours().toString().padStart(2, '0');
         let min = todaysDate.getMinutes().toString().padStart(2, '0');
         let sec = todaysDate.getSeconds().toString().padStart(2, '0');
+
     
         let currentTime =  display.innerText = hr + ':' + min + ':' + sec;
        
-       if(isNaN(alarmHours) || isNaN(alarmMinutes) || isNaN(alarmSeconds)){
+       if(isNaN(alarmHours) || isNaN(alarmMinutes)){
         output.innerText = 'Refresh and enter a valid Number!'
 
         } else if (alarmTime === currentTime){
@@ -73,7 +73,7 @@ onEvent('click', setAlarm, function(){
             let element = document.body;
             element.classList.toggle("dark-mode");
 
-        } else if (alarmHours >= 24 || alarmMinutes >= 60 || alarmSeconds >= 60) {
+        } else if (alarmHours >= 24 || alarmMinutes >= 60) {
             output.innerText = 'Refresh and enter an hour between 0-23!'
 
         } else 
